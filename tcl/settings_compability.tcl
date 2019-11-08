83e03528d18155d5dd3865a02993a6e6dcc65892
aherkle <andreas.herkle@uni-ulm.de>
2019-11-08 16:57:18 +0100

    added microzed baord
    switched to common/individual settings

--- tcl/settings_compability.tcl
+++ tcl/settings_compability.tcl
@@ -1,7 +1,7 @@
 
 proc check_board_supported {} {
 	
-	set property_board [get_property BOARD [current_project]]
+	set property_board [get_property BOARD_PART [current_project]]
 	
 	if {$property_board == "digilentinc.com:zybo:part0:1.0"} {
 		puts "supported board found: zybo"
@@ -11,6 +11,8 @@ proc check_board_supported {} {
 		puts "supported board found: pynq"
 	} elseif {$property_board == "digilentinc.com:zybo-z7-20:part0:1.0"} {
 		puts "supported board found: zybo-z7-20"
+	} elseif {$property_board == "em.avnet.com:microzed_7020:part0:1.1"} {
+		puts "supported board found: microzed"
 	} else {
 		error "board not supported: $property_board"
 	}
@@ -18,20 +20,27 @@ proc check_board_supported {} {
 
 proc ps_apply_board_settings {} {
 	
-	set property_board [get_property BOARD [current_project]]
+	# common settings
+	set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1} CONFIG.PCW_USE_FABRIC_INTERRUPT {1} CONFIG.PCW_S_AXI_HP0_DATA_WIDTH {32} CONFIG.PCW_USE_M_AXI_GP0 {1} CONFIG.PCW_IRQ_F2P_INTR {1}  CONFIG.PCW_USB0_PERIPHERAL_ENABLE {0}] [get_bd_cells processing_system7_0]
+	
+	# individual settings	
+	set property_board [get_property BOARD_PART [current_project]]
 	
 	if {$property_board == "digilentinc.com:zybo:part0:1.0"} {
 		# zybo
-		set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1} CONFIG.PCW_USE_FABRIC_INTERRUPT {1} CONFIG.PCW_S_AXI_HP0_DATA_WIDTH {32} CONFIG.PCW_IRQ_F2P_INTR {1} CONFIG.PCW_QSPI_PERIPHERAL_ENABLE {0} CONFIG.PCW_ENET0_PERIPHERAL_ENABLE {1} CONFIG.PCW_TTC0_PERIPHERAL_ENABLE {0} CONFIG.PCW_USB0_PERIPHERAL_ENABLE {0}] [get_bd_cells processing_system7_0]
+		set_property -dict [list CONFIG.PCW_ENET0_PERIPHERAL_ENABLE {1} CONFIG.PCW_TTC0_PERIPHERAL_ENABLE {0}] [get_bd_cells processing_system7_0]
 	} elseif {$property_board == "em.avnet.com:zed:part0:1.4" || $property_board == "digilentinc.com:zedboard:part0:1.0"} {
 		# zedboard
-		set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1} CONFIG.PCW_USE_FABRIC_INTERRUPT {1} CONFIG.PCW_S_AXI_HP0_DATA_WIDTH {32} CONFIG.PCW_IRQ_F2P_INTR {1} CONFIG.PCW_QSPI_PERIPHERAL_ENABLE {0} CONFIG.PCW_TTC0_PERIPHERAL_ENABLE {0} CONFIG.PCW_USB0_PERIPHERAL_ENABLE {0}] [get_bd_cells processing_system7_0]
+		set_property -dict [list CONFIG.PCW_TTC0_PERIPHERAL_ENABLE {0}] [get_bd_cells processing_system7_0]
 	} elseif {$property_board == "www.digilentinc.com:pynq-z1:part0:1.0"} {
 		# pynq
-		set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1} CONFIG.PCW_USE_FABRIC_INTERRUPT {1} CONFIG.PCW_S_AXI_HP0_DATA_WIDTH {32} CONFIG.PCW_IRQ_F2P_INTR {1} CONFIG.PCW_QSPI_PERIPHERAL_ENABLE {0} CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE {1} CONFIG.PCW_USB0_PERIPHERAL_ENABLE {0}] [get_bd_cells processing_system7_0]
+		set_property -dict [list CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE {1}] [get_bd_cells processing_system7_0]
 	} elseif {$property_board == "digilentinc.com:zybo-z7-20:part0:1.0"} {
 		# zybo-z7-20
-		set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1} CONFIG.PCW_USE_FABRIC_INTERRUPT {1} CONFIG.PCW_S_AXI_HP0_DATA_WIDTH {32} CONFIG.PCW_IRQ_F2P_INTR {1} CONFIG.PCW_QSPI_PERIPHERAL_ENABLE {0} CONFIG.PCW_USB0_PERIPHERAL_ENABLE {0}] [get_bd_cells processing_system7_0]
+		# set_property -dict [list ] [get_bd_cells processing_system7_0]
+	} elseif {$property_board == "em.avnet.com:microzed_7020:part0:1.1"} {
+		# microzed
+		set_property -dict [list CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE {1} CONFIG.PCW_WDT_PERIPHERAL_ENABLE {0} CONFIG.PCW_TTC0_PERIPHERAL_ENABLE {0} ] [get_bd_cells processing_system7_0]
 	} else {
 		error "can't create ZYNQ PS - board not supported"
 	}
